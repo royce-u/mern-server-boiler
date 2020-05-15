796a9fa15483b66245b8bb6adedf66da9d714281
royce-u <royceubando@gmail.com>
2020-05-15 09:27:46 -0700

    finished auth/login

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,30 @@ let jwt = require('jsonwebtoken')
 // POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/login')
+  //look up user by their email
+  dbUser.findOne({ email: req.body.email })
+  .then(user => {
+    //check whether the user exists
+    if (user){
+      //they don't have an account.  Send error message
+      return res.status(404).send({ message: 'user not found' })
+    }
+    //they exists but make sure password is correct
+    if(!user.validPassword(req.body.password)){
+      //incorrect password - send error back
+      req.status(401).send({message: 'Invalid Credentials'})
+    }
+
+    //we have a good user - make them a new token, sent it to them
+    let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+      expiresIn: 60 * 60 * 8//8 hours, in seconds
+    })
+    res.send({ token })
+  })
+  .catch(err => {
+    console.log('Error in post auth login', err)
+    res.status(503).send({ message: 'Server-side error'})
+  })
 })
 
 // POST to /auth/signup (create user; generate token)
