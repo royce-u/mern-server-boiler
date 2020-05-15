c93a4004633f6efaf8435499393cec0e32993547
royce-u <royceubando@gmail.com>
2020-05-15 10:15:57 -0700

    added cors

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 // Require needed packages
 require('dotenv').config()
+var cors = require('cors')
 let express = require('express')
 let morgan = require('morgan')
 let rowdyLogger = require('rowdy-logger')
+let expressJwt = require('express-jwt')
 
 // Instantiate app
 let app = express()
 let rowdyResults = rowdyLogger.begin(app)
 
 // Set up middleware
+//morgan - tellings us it hits the route
+  // POST /auth/login 200 343.884 ms - 393
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false })) // Accept form data
 app.use(express.json()) // Accept data from fetch (or any AJAX call)
+app.use(cors({})) //TODO: add react app as origin for CORS
 
 
 // Routes
 app.use('/auth', require('./controllers/auth'))
-app.use('/profile', require('./controllers/profile'))
+app.use('/profile', expressJwt({ secret: process.env.JWT_SECRET}), require('./controllers/profile'))
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' })
